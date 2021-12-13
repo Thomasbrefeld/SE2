79fb8c166bc74d05a56e659c0e4adde4e4bca0d6
Thomas <Thomas.brefeld@gmail.com>
2021-12-12 19:27:25 -0500

    Added model for Reminder, updated main page.

--- SE2/MainWindow.xaml.cs
+++ SE2/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Text.Json;
 
 namespace SE2
 {
@@ -20,27 +21,6 @@ namespace SE2
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    public class Reminder
-    {
-        private string name;
-        private DateTime dateTime;
-
-        public Reminder(string n, DateTime d)
-        {
-            name = n;
-            dateTime = d;
-        }
-
-        public string getName()
-        {
-            return name;
-        }
-
-        public DateTime getTime()
-        {
-            return dateTime;
-        }
-    }
 
     public partial class MainWindow : Window
     {
@@ -114,6 +94,16 @@ namespace SE2
 
                 eventStackPanel.Children.Add(g);
             }
+        }
+
+        private void loadData()
+        {
+        }
+
+        public void saveData()
+        {
+            //string json = JsonSerializer.Serialize<Reminder>(reminders);
+
         }
 
         private void timerTick(object sender, EventArgs e)
---
+++ SE2/Models/Reminder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE2
+{
+    public class Reminder
+    {
+        private string name;
+        private DateTime dateTime;
+
+        public Reminder(string n, DateTime d)
+        {
+            name = n;
+            dateTime = d;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public DateTime getTime()
+        {
+            return dateTime;
+        }
+    }
+}
